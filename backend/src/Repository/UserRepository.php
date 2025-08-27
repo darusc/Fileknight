025e06f074e454dcca78390c09b7c112ce7aa1ee
Darius <dariuscirneala05@gmail.com>
2025-08-28 00:05:47 +0300

    backend/admin: Added admin dashboard UI

--- backend/src/Repository/UserRepository.php
+++ backend/src/Repository/UserRepository.php
@@ -12,4 +12,44 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($managerRegistry, User::class);
     }
+
+    /**
+     * Gets all the users who were just created and the account
+     * is still pending (the user didn't register yet).
+     *
+     * Ordered ascending by username
+     */
+    public function findPendingCreationFinish(): array
+    {
+        return $this->findBy(['password' => null], ['username' => 'ASC']);
+    }
+
+    /**
+     * Gets all the users who requested a password reset.
+     *
+     * Ordered ascending by username
+     */
+    public function findRequestedReset(): array
+    {
+        return $this->findBy(['resetRequired' => true], ['username' => 'ASC']);
+    }
+
+    /**
+     * Gets the remaining users that didn't request a password reset
+     * and are not newly created
+     */
+    public function findNonSpecial(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.password IS NOT NULL')
+            ->andWhere('u.resetRequired = false')
+            ->orderBy('u.username', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getCount(): int
+    {
+        return count($this->findAll());
+    }
 }
